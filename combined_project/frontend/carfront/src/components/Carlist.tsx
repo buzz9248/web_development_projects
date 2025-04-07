173e1aa3efa76ebad7b9f88fe8424c786efc0ab1
buzz9248 <buzz9248@naver.com>
2025-04-07 15:35:48 +0900

    feat: 20250404 delte function finished

--- combined_project/frontend/carfront/src/components/Carlist.tsx
+++ combined_project/frontend/carfront/src/components/Carlist.tsx
@@ -1,42 +1,62 @@
-import React from 'react'
-import { CarResponse } from '../types';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCars, deleteCar } from "../api/carapi";
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
 
 function Carlist() {
-  const getCars = async (): Promise<CarResponse[]> => {
-    const response = await axios.get('http://localhost:8080/api/cars');
-
-    return response.data._embedded.cars;
-  }
-
+  const queryClient = useQueryClient();
   const { data, error, isSuccess } = useQuery({
-    queryKey: ['cars'],
+    queryKey: ["cars"],
     queryFn: getCars
   });
 
+  const { mutate } = useMutation(deleteCar, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cars'] });
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
+  const columns: GridColDef[] = [
+    { field: 'brand', headerName: 'Brand', width: 200 },
+    { field: 'model', headerName: 'Model', width: 200 },
+    { field: 'color', headerName: 'Color', width: 200 },
+    { field: 'registrationNumber', headerName: 'Reg.nr.', width: 150 },
+    { field: 'modelYear', headerName: 'Model Year', width: 150 },
+    { field: 'price', headerName: 'Price', width: 150 },
+    {
+      field: 'edit',
+      headerName: '',
+      width: 90,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) =>
+        <button
+          onClick={() => mutate(params.row._links.car.href)}
+        >
+          삭제
+        </button>
+    }
+  ]
+
+
+
   if (!isSuccess) {
-    return <span>Loading... 😂</span>
-  } else if (error) {
-    return <span>자동차 데이터 가져오기 중 오류 발생 😭</span>
-  } else {
+    return <span>Loading 중... 😂</span>
+  }
+  else if (error) {
+    return <span>자동차 데이터 가져오기 중 오류 발생 😪</span>
+  }
+  else {
     return (
-      <table>
-        <tbody>
-          {
-            data.map((car: CarResponse) =>
-              <tr key={car._links.self.href}>
-                <td>{car.brand}</td>
-                <td>{car.model}</td>
-                <td>{car.color}</td>
-                <td>{car.registrationNumber}</td>
-                <td>{car.modelYear}</td>
-                <td>{car.price}</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        getRowId={row => row._links.self.href}
+      // disableRowSelectionOnClick={true}
+      />
     );
   }
 }
