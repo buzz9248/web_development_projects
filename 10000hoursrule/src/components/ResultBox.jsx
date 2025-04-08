8003a5ede1f9ee255e707a653e2e03e2c685a8d1
buzz9248 <buzz9248@naver.com>
2025-04-08 20:44:18 +0900

    Feat: react-ize project finished

--- 10000hoursrule/src/components/ResultBox.jsx
+++ 10000hoursrule/src/components/ResultBox.jsx
@@ -1,21 +1,37 @@
 import { Box, Typography, Button } from "@mui/material";
 import LoadingImage from '../assets/loading.png';
 
-const ResultBox = ({loading, resultVisible, field, time, onModalOpen, onShare}) => {
+const ResultBox = ({ loading, resultVisible, field, time, onModalOpen, onShare }) => {
   const calculatedDays = time ? Math.ceil(10000 / Number(time)) : 0;
 
-  return(
+  return (
     <>
       {loading && (
-        <Box sx={{display:'flex', justifyContent: 'center', mb: 8}}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mb: 8 }}>
           <img src={LoadingImage} alt="loading" style={{
             animation: 'rotate 1.1s infinite linear',
             width: 100,
-          }}/>
+          }} />
         </Box>
-
-        // 여기서부터 내일하겠습니다.
       )}
+
+      {resultVisible && (
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', lineHeight: 1.5, mb: 8, }}>
+          <Typography sx={{ fontSize: 20 }}>당신은 <span style={{ fontSize: 70 }}>
+            {field}</span> 전문가가 되기 위해서</Typography>
+          <Typography sx={{ fontSize: 20 }}>대략 <span style={{ fontSize: 70 }}>
+            {calculatedDays}</span>일 이상 훈련하셔야 합니다!! 😳</Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 7 }}>
+            <Button variant="contained" onClick={onModalOpen} sx={{ backgroundColor: '#fcee21', color: '#5b2386', mr: 2 }}>
+              훈련하러 가기 💻
+            </Button>
+            <Button variant="outlined" onClick={onShare} sx={{ backgroundColor: '#fff', color: '#5b2386' }}>
+              Share
+            </Button>
+          </Box>
+        </Box>
+      )}
+
     </>
   );
 }
